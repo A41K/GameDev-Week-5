4b76e9163af245a80f1a131be7feae6a96647c1d
A41K <arokandris@gmail.com>
2026-04-20 21:44:09 +0200

    v1.0 The full release
    - Finished LvL2
    - Added Level labels to the levels
    - Changed a few positions of objects
    - Changed project Width and Height to 1250x720
    
    -----------------------------------------
    
    - Added title screen from platformer (Cause I reuse it)
    - Added GrapeSoda font
    - Started LVL3
    - Removed Unused LVL4 and 5
    - Added Lvl2 -> Lvl 3 flag
    - Finished LVL3
    - Made LVL3 the last level
    - Made the spikes into a scene so its easier to place them
    - Added tutorial level
    - Modified tutorial level
    - Removed checkpoints that were broken
    
    Ship part
    --------------------------------
    - Added the HTML file
    - Added the game.zip
    - Added the exe

---
+++ game-dev-week-5/scripts/next_scene2.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _ready() -> void:
+	# This automatically connects the signal so you don't have to do it in the Godot Editor
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		GameController.save_coins()
+		GameController.clear_respawn_point()
+		get_tree().change_scene_to_file("res://scenes/level_3.tscn")
